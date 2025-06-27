72e90e4b328f7ecedc30af95c22fc02a282e197a
Wajeeha Nadeem <150377263+wajeehanadeem649@users.noreply.github.com>
2025-06-27 16:35:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def recommendations():
                                    random_price=random.choice(price))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
